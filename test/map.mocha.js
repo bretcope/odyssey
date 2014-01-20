81d713a79abcdef9b68387e6b70bc8bf49affebe
Bret Copeland <bret@atlantisflight.org>
2014-01-20 17:30:20 -0500

    Added async.map and fixed a sync bug with parallel.

---
+++ test/map.mocha.js
@@ -0,0 +1,48 @@
+"use strict";
+
+var assert = require('assert');
+var odyssey = require('../');
+
+var async = odyssey.async;
+var httpLog = odyssey.httpLog;
+
+suite('Map', function ()
+{
+	test('Sync Example', function (done)
+	{
+		async.map
+		(
+			[ 'one', 'two', 'three'],
+			function(cb, item, index)
+			{
+				cb(null, 'Hey ' + index + ' ' + item);
+			},
+			function (hlog, results)
+			{
+				assert(!hlog.failed);
+				assert(results[1] === 'Hey 1 two');
+
+				done();
+			}
+		);
+	});
+	
+	test('Async Example', function (done)
+	{
+		async.map
+		(
+			[ 'one', 'two', 'three'],
+			function(cb, item, index)
+			{
+				process.nextTick(function () { cb(null, 'Hey ' + index + ' ' + item); });
+			},
+			function (hlog, results)
+			{
+				assert(!hlog.failed);
+				assert(results[1] === 'Hey 1 two');
+
+				done();
+			}
+		);
+	});
+});
